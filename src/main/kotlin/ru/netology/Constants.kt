a150ad974021539e8184d9de9fa72c406cc84788
vanya <whitebeltvanya@gmail.com>
2025-08-15 03:19:37 +0300

    Initial commit

---
+++ src/main/kotlin/ru/netology/Constants.kt
@@ -0,0 +1,21 @@
+package ru.netology
+
+const val MINUTE_SECOND = 60
+const val HOUR_SECOND = 3600
+const val DAY_SECOND = 86400
+
+val MINUTE_ARR_FIRST = listOf(1, 21, 31, 41, 51)
+const val MINUTE_ARR_FIRST_DESCR = "минуту"
+
+val MINUTE_ARR_SECOND = listOf(2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54)
+const val MINUTE_ARR_SECOND_DESCR = "минуты"
+
+const val MINUTE_DEFAULT_DESCR = "минут"
+
+val HOUR_ARR_FIRST = listOf(1, 21)
+const val HOUR_ARR_FIRST_DESCR = "час"
+
+val HOUR_ARR_SECOND = listOf(2, 3, 4, 22, 23, 24)
+const val HOUR_ARR_SECOND_DESCR = "часа"
+
+const val HOUR_DEFAULT_DESCR = "часов"
---
+++ src/main/kotlin/ru/netology/Main.kt
@@ -0,0 +1,43 @@
+package ru.netology
+
+fun main() {
+
+    val visitInSecond = listOf(45, 60, 700, 3600, 4500, 75_430, 86_400, 172_800, 259_200, 1000_000)
+
+    for (seconds in visitInSecond) {
+        println(agoToText(seconds))
+    }
+
+}
+
+fun agoToText(seconds: Int): String {
+    val hours = seconds / HOUR_SECOND
+    val minutes = (seconds % HOUR_SECOND) / MINUTE_SECOND
+
+    val result = when {
+        seconds in 0..60 -> "был(а) только что"
+        seconds in 61 until HOUR_SECOND -> "был(а) $minutes ${getMinuteDescr(minutes)} назад"
+        seconds in HOUR_SECOND..DAY_SECOND ->
+            "был(а) $hours ${getHourDescr(hours)} $minutes ${getMinuteDescr(minutes)} назад"
+
+        seconds in DAY_SECOND + 1..2 * DAY_SECOND -> "был(а) вчера"
+        seconds in 2 * DAY_SECOND + 1..3 * DAY_SECOND -> "был(а) позавчера"
+        seconds > 3 * DAY_SECOND -> "был(а) давно"
+        else -> "Некорректное значение"
+    }
+
+    return result
+}
+
+
+fun getHourDescr(hour: Int): String = when (hour) {
+    in HOUR_ARR_FIRST -> HOUR_ARR_FIRST_DESCR
+    in HOUR_ARR_SECOND -> HOUR_ARR_SECOND_DESCR
+    else -> HOUR_DEFAULT_DESCR
+}
+
+fun getMinuteDescr(minutes: Int): String = when (minutes) {
+    in MINUTE_ARR_FIRST -> MINUTE_ARR_FIRST_DESCR
+    in MINUTE_ARR_SECOND -> MINUTE_ARR_SECOND_DESCR
+    else -> MINUTE_DEFAULT_DESCR
+}
